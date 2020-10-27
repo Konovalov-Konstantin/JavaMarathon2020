b9b7c6852d24f6fb97073c191a98d5d89ce00512
Konovalov Konstantin <kon-kos87@yandex.ru>
2020-10-27 19:38:16 +0500

    complete day 2 tasks

--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -1,7 +1,24 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        int a, b;
+        System.out.println("Ведите два числа");
+        Scanner sc = new Scanner(System.in);
 
+        a = sc.nextInt();
+        b = sc.nextInt();
+        if (a >= b) {
+            System.out.println("Некорректный ввод");
+        } else {
+            while (b > a) {
+                if (a % 5 == 0 && a % 10 != 0) {
+                    System.out.print(a + " ");
+                }
+                a++;
+            }
+        }
     }
 }
--- src/main/java/day2/Task4.java
+++ src/main/java/day2/Task4.java
@@ -1,7 +1,20 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task4 {
     public static void main(String[] args) {
-
+        double y;
+        System.out.println("Ведите число X");
+        Scanner sc = new Scanner(System.in);
+        double x = sc.nextDouble();
+        if (x >= 5) {
+            y = (x * x - 10) / (x + 7);
+        } else if (x > -3 && x < 5) {
+            y = (x + 3) * (x * x - 2);
+        } else {
+            y = 420;
+        }
+        System.out.println("Result is " + y);
     }
 }
