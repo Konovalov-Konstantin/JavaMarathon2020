9241ca6850ca4689844c25766127969f99947c34
Konovalov Konstantin <kon-kos87@yandex.ru>
2020-11-04 20:45:42 +0500

    day 8

---
+++ src/main/java/day8/Airplane.java
@@ -0,0 +1,24 @@
+package day8;
+
+public class Airplane {
+    private String producer;
+    private int year, length, weight, fuel;
+
+    Airplane(String producer, int year, int length, int weight) {
+        int fuel;
+        this.producer = producer;
+        this.year = year;
+        this.length = length;
+        this.weight = weight;
+    }
+    @Override
+    public String toString () {
+        return ("Изготовитель " + producer +"\n"+ "Год выпуска " + year+"\n" + "Длина " + length+"\n"+
+                "Вес " + weight);
+    }
+
+    public static void main(String[] args) {
+        Airplane airplane = new Airplane("Boeing", 2005, 35, 20_000);
+        System.out.println(airplane.toString());
+    }
+}
--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,25 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        long startConcat= System.currentTimeMillis();
+        for (int i = 0; i<=20_000; i++) {
+            System.out.printf("%d ",i);
+        }
+        long finishConcat = System.currentTimeMillis();
+        long timeConcat = finishConcat - startConcat;
+        System.out.println();
 
+        StringBuilder stringBuilder = new StringBuilder();
+        long startSB= System.currentTimeMillis();
+        for (int i = 0; i <= 20_000; i++) {
+            stringBuilder.append(i + " ");
+        }
+        System.out.print(stringBuilder);
+        long finishSB = System.currentTimeMillis();
+        long timeSB = finishSB - startSB;
+        System.out.println();
+        System.out.println("Время выполнения программы с конкатенацией " + timeConcat + " мс");
+        System.out.println("Время выполнения программы StringBuilder " + timeSB + " мс");
+        System.out.println("Разница = " + (timeConcat - timeSB)+ " мс");
     }
 }
