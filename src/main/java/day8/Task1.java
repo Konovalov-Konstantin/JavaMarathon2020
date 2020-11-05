9e0ed37d2e7088ba01a7b731bd4caf02dad7f809
Konovalov Konstantin <kon-kos87@yandex.ru>
2020-11-05 21:08:35 +0500

    day 8 correct

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,10 +2,13 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        String s = new String("0");
+
         long startConcat= System.currentTimeMillis();
-        for (int i = 0; i<=20_000; i++) {
-            System.out.printf("%d ",i);
+        for (int i = 1; i<=20_000; i++) {
+            s = s + " "+ i;
         }
+        System.out.println(s);
         long finishConcat = System.currentTimeMillis();
         long timeConcat = finishConcat - startConcat;
         System.out.println();
