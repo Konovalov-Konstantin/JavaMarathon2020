d8b228d5bf07ce1992e25c6134e0cdb5debd7186
Konovalov Konstantin <kon-kos87@yandex.ru>
2020-11-15 21:28:15 +0500

    day 15

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,30 @@
 package day15;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
+public class Task1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("shoes.csv");
+        File file1 = new File("missing_shoes.txt");
+        PrintWriter pw = new PrintWriter(file1);
+        pw.println("Отсутствуют на складе: " + "\n");
+        int i = 1;
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] element = line.split(";");
+                if (Integer.parseInt(element[2]) == 0) {
+                    pw.println(i + " " + element[0] + " размер - " + element[1]);
+                    i++;
+                }
+            }
+            pw.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
     }
 }
